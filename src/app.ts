06115a8f91505cfec2a7d1ce6f2822e712b6dbf2
Robbie <Robbie@email.com>
2024-09-11 20:41:28 +0900

    Fix: package.json sciripts 수정, dotenv import 방식 변경 Add: .env.example 추가 #1

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config(); // .env 파일에 정의된 환경 변수 로드하기
+import "dotenv/config";
 
 const app = express(); // express 생성
-const port = process.env.PORT; // 환경 변수에서 포트 번호 가져오기
+const SERVER_PORT = process.env.SERVER_PORT; // 환경 변수에서 포트 번호 가져오기
 
 // Health Check
 app.get("/health-check", (req, res) => {
@@ -12,6 +10,6 @@ app.get("/health-check", (req, res) => {
 });
 
 // 서버 시작
-app.listen(port, () => {
-  console.log(`${port}포트에서 실행 됐습니다.`);
+app.listen(SERVER_PORT, () => {
+  console.log(`${SERVER_PORT}포트에서 실행 됐습니다.`);
 });
