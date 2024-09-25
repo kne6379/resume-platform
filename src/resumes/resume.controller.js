b8e69ab8d53f03f748446552a0baa339743dc723
NohSiHeon <shtlgjs016@naver.com>
2024-09-25 22:31:43 +0900

    Refactor: 각 user API에 RO 적용, 메시지 상수화 추가, RO 변수명 수정 #5

--- src/resumes/resume.controller.js
+++ src/resumes/resume.controller.js
@@ -1,130 +1,130 @@
 import { HTTP_STATUS } from "../constants/http-status.constants.js";
 import { MESSAGES } from "../constants/message.constants.js";
-import { sucessResponse } from "../utils/response-helper.js";
+import { successResponse } from "../utils/response-helper.js";
 
 class ResumeController {
-  constructor(resumeService) {
-    this.resumeService = resumeService;
-  }
-  createResume = async (req, res, next) => {
-    try {
-      const userId = req.user.id;
-      const { title, bio } = req.body;
-      const data = await this.resumeService.createResume(userId, title, bio);
+	constructor(resumeService) {
+		this.resumeService = resumeService;
+	}
+	createResume = async (req, res, next) => {
+		try {
+			const userId = req.user.id;
+			const { title, bio } = req.body;
+			const data = await this.resumeService.createResume(userId, title, bio);
 
-      return sucessResponse({
-        res,
-        status: HTTP_STATUS.CREATED,
-        message: MESSAGES.RESUMES.CREATED.SUCCEED,
-        data,
-      });
-    } catch (error) {
-      res.json({ error });
-    }
-  };
+			return successResponse({
+				res,
+				status: HTTP_STATUS.CREATED,
+				message: MESSAGES.RESUMES.CREATED.SUCCEED,
+				data,
+			});
+		} catch (error) {
+			res.json({ error });
+		}
+	};
 
-  getResumes = async (req, res, next) => {
-    try {
-      const userId = req.user.id;
-      const data = await this.resumeService.getResumes(userId);
+	getResumes = async (req, res, next) => {
+		try {
+			const userId = req.user.id;
+			const data = await this.resumeService.getResumes(userId);
 
-      return sucessResponse({
-        res,
-        message: MESSAGES.RESUMES.READ_LIST.SUCCEED,
-        data,
-      });
-    } catch (error) {
-      res.json({ error });
-    }
-  };
+			return successResponse({
+				res,
+				message: MESSAGES.RESUMES.READ_LIST.SUCCEED,
+				data,
+			});
+		} catch (error) {
+			res.json({ error });
+		}
+	};
 
-  getResumeById = async (req, res, next) => {
-    try {
-      const { userId } = req.body;
-      const resumeId = +req.params.id;
-      const data = await this.resumeService.getResumeById(resumeId, userId);
+	getResumeById = async (req, res, next) => {
+		try {
+			const { userId } = req.body;
+			const resumeId = +req.params.id;
+			const data = await this.resumeService.getResumeById(resumeId, userId);
 
-      return sucessResponse({
-        res,
-        message: MESSAGES.RESUMES.READ_DETAIL.SUCCEED,
-        data,
-      });
-    } catch (error) {
-      res.json({ error });
-    }
-  };
+			return successResponse({
+				res,
+				message: MESSAGES.RESUMES.READ_DETAIL.SUCCEED,
+				data,
+			});
+		} catch (error) {
+			res.json({ error });
+		}
+	};
 
-  updateResume = async (req, res, next) => {
-    try {
-      const resumeId = +req.params.id;
-      const { userId, title, bio } = req.body;
-      const data = await this.resumeService.updateResume(
-        resumeId,
-        userId,
-        title,
-        bio
-      );
+	updateResume = async (req, res, next) => {
+		try {
+			const resumeId = +req.params.id;
+			const { userId, title, bio } = req.body;
+			const data = await this.resumeService.updateResume(
+				resumeId,
+				userId,
+				title,
+				bio
+			);
 
-      return sucessResponse({
-        res,
-        message: MESSAGES.RESUMES.UPDATE.SUCCEED,
-        data,
-      });
-    } catch (error) {
-      res.json({ error });
-    }
-  };
+			return successResponse({
+				res,
+				message: MESSAGES.RESUMES.UPDATE.SUCCEED,
+				data,
+			});
+		} catch (error) {
+			res.json({ error });
+		}
+	};
 
-  deleteResume = async (req, res, next) => {
-    try {
-      const resumeId = +req.params.id;
-      const { userId } = req.user.id;
+	deleteResume = async (req, res, next) => {
+		try {
+			const resumeId = +req.params.id;
+			const { userId } = req.user.id;
 
-      await this.resumeService.deleteResume(resumeId, userId);
+			await this.resumeService.deleteResume(resumeId, userId);
 
-      return sucessResponse({
-        res,
-        message: MESSAGES.RESUMES.DELETE.SUCCEED,
-      });
-    } catch (error) {
-      res.json({ error });
-    }
-  };
+			return successResponse({
+				res,
+				message: MESSAGES.RESUMES.DELETE.SUCCEED,
+			});
+		} catch (error) {
+			res.json({ error });
+		}
+	};
 
-  updateResumeStatus = async (req, res, next) => {
-    try {
-      const modifier = req.user.name;
-      const resumeId = +req.params.id;
-      const { newStatus } = req.body;
-      const data = await this.resumeService.updateResumeStatus(
-        modifier,
-        resumeId,
-        newStatus
-      );
-      return sucessResponse({
-        res,
-        message: MESSAGES.RESUMES.LOGGED_STATUS_CHANGE,
-        data,
-      });
-    } catch (error) {
-      res.json({ error });
-    }
-  };
+	updateResumeStatus = async (req, res, next) => {
+		try {
+			const modifier = req.user.name;
+			const resumeId = +req.params.id;
+			const { newStatus } = req.body;
+			const data = await this.resumeService.updateResumeStatus(
+				modifier,
+				resumeId,
+				newStatus
+			);
+			return successResponse({
+				res,
+				message: MESSAGES.RESUMES.LOGGED_STATUS_CHANGE,
+				data,
+			});
+		} catch (error) {
+			res.json({ error });
+		}
+	};
 
-  getResumeStatusLogs = async (req, res, next) => {
-    try {
-      const resumeId = +req.params.id;
-      const data = await this.resumeService.getResumeStatusLogs(resumeId);
+	getResumeStatusLogs = async (req, res, next) => {
+		try {
+			const resumeId = +req.params.id;
+			const data = await this.resumeService.getResumeStatusLogs(resumeId);
 
-      return sucessResponse({
-        res,
-        message: MESSAGES.RESUMES.READ_STATUS_LOGS,
-        data,
-      });
-    } catch (error) {
-      res.json({ error });
-    }
-  };
+			return successResponse({
+				res,
+				message: MESSAGES.RESUMES.READ_STATUS_LOGS,
+				data,
+			});
+		} catch (error) {
+			res.json({ error });
+		}
+	};
 }
 
 export { ResumeController };
