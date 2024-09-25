42dc9206a9c8b490cd74f591d2d2fafdd9e2a28f
NohSiHeon <shtlgjs016@naver.com>
2024-09-25 17:00:37 +0900

    Add: Joi 로그인 스키마 구현

--- src/auth/auth.router.js
+++ src/auth/auth.router.js
@@ -3,12 +3,13 @@ import { AuthController } from "./auth.controller.js";
 import { AuthService } from "./auth.service.js";
 import { prisma } from "../configs/prisma-client.js";
 import { signUpValidator } from "../middlewares/validators/signUp-validator.js";
+import { signInValidator } from "../middlewares/validators/signIn-validator.js";
 
 const authRouter = express.Router();
 const authService = new AuthService(prisma);
 const authController = new AuthController(authService);
 
 authRouter.post("/sign-up", signUpValidator, authController.signUp);
-authRouter.post("/sign-in", authController.signIn);
+authRouter.post("/sign-in", signInValidator, authController.signIn);
 
 export { authRouter };
--- src/middlewares/validators/signUp-validator.js
+++ src/middlewares/validators/signUp-validator.js
@@ -1,20 +1,21 @@
 import Joi from "joi";
+import { MESSAGES } from "../../constants/message.constants.js";
 
 const signUpSchema = Joi.object({
 	email: Joi.string().email().required().messages({
-		"any.required": "이메일을 입력해주세요.",
-		"string.email": "이메일 형식이 아닙니다."
+		"any.required": MESSAGES.AUTH.COMMON.EMAIL.REQUIRED,
+		"string.email": MESSAGES.AUTH.COMMON.EMAIL.INVALID_FORMAT
 	}),
 	password: Joi.string().min(6).required().messages({
-		"any.required": "비밀번호를 입력해주세요.",
+		"any.required": MESSAGES.AUTH.COMMON.PASSWORD.REQURIED,
 		"string.min": "비밀번호는 6자리 이상이어야 합니다."
 	}),
 	passwordConfirm: Joi.string().required().valid(Joi.ref('password')).messages({
-		"any.required": "비밀번호 확인을 입력해주세요.",
-		"any.only": "패스워드가 일치하지 않습니다."
+		"any.required": MESSAGES.AUTH.COMMON.PASSWORD_CONFIRM.REQURIED,
+		"any.only": MESSAGES.AUTH.COMMON.PASSWORD_CONFIRM.NOT_MACHTED_WITH_PASSWORD
 	}),
 	name: Joi.string().min(2).required().messages({
-		"any.required": "이름을 입력해주세요.",
+		"any.required": MESSAGES.AUTH.COMMON.NAME.REQURIED,
 		"string.min": "이름은 최소 2자리 이상어야 합니다."
 	}),
 });
