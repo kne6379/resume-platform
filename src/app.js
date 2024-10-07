a5ae6b41d471ff7d93557aae82dd6bd6177c810a
Robbie <Robbie@email.com>
2024-10-07 22:37:44 +0900

    Refactor: 로그 미들웨어 및 사용처 별로 주석 추가, app.js 및 morgan 미들웨어 내용 수정 #11

--- src/app.js
+++ src/app.js
@@ -6,14 +6,16 @@ import { errorMiddleware } from "./middlewares/error.middleware.js";
 import { sessionConfig } from "./configs/session.config.js";
 import { redisStore } from "./configs/redis.config.js";
 import { morganMiddleware } from "./middlewares/morgan.middleware.js";
+import { logger } from "./configs/logger.config.js";
 
 const app = express(); // express 생성
 const SERVER_PORT = process.env.SERVER_PORT; // 환경 변수에서 포트 번호 가져오기
 
-// json, url 파싱
+/// json, url 파싱
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// 세션 설정 (Redis 세션 저장소를 사용)
 app.use(expressSession({ ...sessionConfig, store: redisStore }));
 
 // Health Check
@@ -21,11 +23,16 @@ app.get("/health-check", (req, res) => {
   res.status(200).send("OK");
 });
 
+// morgan 미들웨어, http 요청 로그 관리
 app.use(morganMiddleware);
+
+// API 라우터 연결
 app.use("/", apiRouter);
+
+// 에러 미들웨어, 에러 발생 시 error 레벨의 로그 기록 및 처리
 app.use(errorMiddleware);
 
-// 서버 시작
+// 서버 시작, info 레벨의 로거를 사용하여 서버가 언제, 몇 번 포트로 시작되었는지 기록
 app.listen(SERVER_PORT, () => {
-  console.log(`${SERVER_PORT}포트에서 실행 됐습니다.`);
+  logger.info(`${SERVER_PORT} 포트에서 서버가 실행되었습니다.`);
 });
--- src/middlewares/morgan.middleware.js
+++ src/middlewares/morgan.middleware.js
@@ -2,21 +2,21 @@ import morgan from "morgan";
 import { logger } from "../configs/logger.config.js";
 import { NODE_ENV } from "../constants/env.constants.js";
 
-const format = () => {
-  const result =
-    NODE_ENV === "production"
-      ? '[:remote-addr - :remote-user] ":method :url HTTP/:http-version" :status :response-time ms - :res[content-length] ":referrer" ":user-agent"'
-      : ":method :url :status :response-time ms - :res[content-length]";
-  return result;
-};
+// morgan에서 사용할 포맷을 정의
+// 개발 환경에서는 간결한 형식, 배포 환경에서는 자세한 로그 형식을 사용
+const format =
+  NODE_ENV === "production"
+    ? '[:remote-addr - :remote-user] ":method :url HTTP/:http-version" :status :response-time ms - :res[content-length] ":referrer" ":user-agent"'
+    : ":method :url :status :response-time ms - :res[content-length]";
 
+// logger.info 레벨로 출력하게 되며, morgan의 로그가 여기에 기록된다.
 const stream = {
   write: message => {
     logger.info(message);
   },
 };
 
-// 상태 코드별 글자색 설정, 현재 에러코드는 출력하지 않으므로 사용하지 않는다.
+// 상태 코드별 글자색 설정
 morgan.token("status", function (req, res) {
   let color;
   if (res.statusCode < 300) color = "\x1B[32m"; //green
@@ -28,6 +28,8 @@ morgan.token("status", function (req, res) {
   return color + res.statusCode + "\x1B[35m"; /*보라색*/
 });
 
-const morganMiddleware = morgan(format(), { stream });
+// morgan 미들웨어 생성
+// morgan에 설정된 포맷과 stream을 적용
+const morganMiddleware = morgan(format, { stream });
 
 export { morganMiddleware };
