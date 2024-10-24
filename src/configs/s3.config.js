e44ef78be8d3667d0f17b8485c8f9309b30903fc
Robbie <Robbie@email.com>
2024-10-25 03:24:54 +0900

    Comment: 주석 추가 #11

--- src/configs/s3.config.js
+++ src/configs/s3.config.js
@@ -1,8 +1,11 @@
 import AWS from "@aws-sdk/client-s3";
 import { AWS_ACCESS_KEY, AWS_REGION, AWS_SECRET_ACCESS_KEY } from "../constants/env.constants.js";
 
+// AWS SDK에서 제공하는 S3 서비스를 사용하기 위해 S3 인스턴스를 생성
 const s3 = new AWS.S3({
+  // S3 리소스가 위치한 리전 설정
   region: AWS_REGION,
+  // S3에 접근하기 위한 자격 증명 설정
   credentials: {
     accessKeyId: AWS_ACCESS_KEY,
     secretAccessKey: AWS_SECRET_ACCESS_KEY,
--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -3,17 +3,20 @@ import multerS3 from "multer-s3";
 import { s3 } from "../configs/s3.config.js";
 import { S3_BUCKET_NAME } from "../constants/env.constants.js";
 
+// multer와 multerS3를 사용하여 파일 업로드 설정을 정의
 const upload = multer({
+  // S3 스토리지 설정을 사용하여 파일을 S3에 업로드
   storage: multerS3({
     s3: s3,
-    bucket: S3_BUCKET_NAME, // 객체를 업로드할 버킷 이름
+    bucket: S3_BUCKET_NAME, // 업로드할 S3 버킷 이름 설정
     key: function (req, file, cb) {
-      // 객체의 키로 고유한 식별자 이기 때문에 겹치면 안됨
+      // S3에 저장될 객체의 키를 설정. 파일 이름이 고유해야 하므로 랜덤 숫자와 현재 시간을 조합하여 중복을 방지.
+      // file.originalname.split(".").pop()은 파일의 확장자를 가져옴
       cb(null, Math.floor(Math.random() * 1000).toString() + Date.now() + "." + file.originalname.split(".").pop());
     },
   }),
 });
-
+// 단일 파일 업로드 미들웨어로 사용하기 위한 설정 ("profileUrl" 필드에서 파일을 업로드)
 const uploadSingleFile = upload.single("profileUrl");
 
 export { uploadSingleFile };
